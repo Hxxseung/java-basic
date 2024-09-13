e933ef48b72e09120706bd840b0201b7094d7abd
Hxxseung <soaorsith2@naver.com>
2024-09-13 12:14:35 +0900

    Java - Basic

--- src/ref/NullMain3.java
+++ src/ref/NullMain3.java
@@ -3,9 +3,9 @@ package ref;
 public class NullMain3 {
     public static void main(String[] args) {
         BigData bigData = new BigData();
-        System.out.println("bigData.count=" + bigData.count);
-        System.out.println("bigData.data=" + bigData.data);
+        System.out.println("bigData.count=" + bigData.count);   //초기값이 0으로 초기화 되서 들어감
+        System.out.println("bigData.data=" + bigData.data); //초기값이 null로 들어감
         //NullPointerException
-        System.out.println("bigData.data.value=" + bigData.data.value);
+        System.out.println("bigData.data.value=" + bigData.data.value); //bigData.null.value로 인식해서 발생
     }
 }
