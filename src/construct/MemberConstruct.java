c40ef5024ac679b2b743f055cccf6fab13de0408
Hxxseung <soaorsith2@naver.com>
2024-09-18 12:51:06 +0900

    Java - Basic

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,6 +5,13 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    //추가
+    MemberConstruct(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.grade = 50;        //성적 값이 없는 Member객체는 성적이 50으로 바뀌게 된다.
+    }
+
     MemberConstruct(String name, int age, int grade) {      //생성자 부분
         System.out.println("생성자 호출 name=" + name + ", age=" + age + ", grade=" + grade);
         this.name = name;
