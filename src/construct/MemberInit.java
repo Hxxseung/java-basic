934a9e5ca2f5fa51765c6d52f7ad2d3beb22765a
Hxxseung <soaorsith2@naver.com>
2024-09-20 10:56:38 +0900

    Java - Basic

--- src/construct/MemberInit.java
+++ src/construct/MemberInit.java
@@ -1,12 +1,12 @@
 package construct;
 
 public class MemberInit {
-    String name;
+    String name;    //멤버변수
     int age;
     int grade;
 
-    void initMember(String name, int age, int grade) {
-        this.name = name;       //가까운 인스턴스부터 호출하기 때문에 this를 통해 클래스의  변수를 호출한다.
+    void initMember(String name, int age, int grade) {//매개변수
+        this.name = name;       //가까운 인스턴스부터 호출하기 때문에 매개변수가 호출되므로, this를 통해 멤버 변수를 호출한다.
         this.age = age;
         this.grade = grade;
     }
