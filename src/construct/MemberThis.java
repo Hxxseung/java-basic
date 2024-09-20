b2911b5b72441eea9b5ceb8a006e78da645a3078
Hxxseung <soaorsith2@naver.com>
2024-09-20 11:01:25 +0900

    Java - Basic

--- src/construct/MemberThis.java
+++ src/construct/MemberThis.java
@@ -4,6 +4,6 @@ public class MemberThis {
     public String nameField;
 
     public void initMember(String nameParameter){
-        nameField = nameParameter;
+        nameField = nameParameter; //nameField에는 매개변수로 없기 때문에 this가 생략되어 표시된다. (this를 넣어도 같은 결과가 나온다)
     }
 }
