ed27cb080f915c0860bc88951d5d941514022c87
Hxxseung <soaorsith2@naver.com>
2024-09-20 10:53:14 +0900

    Java - Basic

--- src/construct/MemberInit.java
+++ src/construct/MemberInit.java
@@ -6,7 +6,7 @@ public class MemberInit {
     int grade;
 
     void initMember(String name, int age, int grade) {
-        this.name = name;
+        this.name = name;       //가까운 인스턴스부터 호출하기 때문에 this를 통해 클래스의  변수를 호출한다.
         this.age = age;
         this.grade = grade;
     }
