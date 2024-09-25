cf116156afdd85b5f6bbf886fc81db522db1acd8
Hxxseung <soaorsith2@naver.com>
2024-09-25 12:14:13 +0900

    DecoMain2 text add

--- src/static2/DecoMain2.java
+++ src/static2/DecoMain2.java
@@ -3,7 +3,7 @@ package static2;
 public class DecoMain2 {
     public static void main(String[] args) {
         String s = "hello java";
-        String deco = DecoUtil2.deco(s);
+        String deco = DecoUtil2.deco(s);    //static을 사용해서 클래스에서 바로 불러오기 가능. 객체생성 필요x
 
         System.out.println("before: " + s);
         System.out.println("after: " + deco);
