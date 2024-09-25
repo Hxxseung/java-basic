6c5778c2a77bae3eb6f03b5b36f94d28a379b0e6
Hxxseung <soaorsith2@naver.com>
2024-09-25 12:18:54 +0900

    오타 수정

--- src/static2/DecoData.java
+++ src/static2/DecoData.java
@@ -5,7 +5,7 @@ public class DecoData {
     private int instanceValue;
     private static int staticValue;
 
-    public static void staticCall(){   //정적 메서드이므로 static만 ㄷ사용 할 수 있음.
+    public static void staticCall(){   //정적 메서드이므로 static만 사용 할 수 있음.
                                         //static이 없는 인스턴스 변수나 인스턴스 메서드에 접근하면 컴파일 오류가 발생함.
         //instanceValue++;  //인스턴스 변수 접근, compile error
         //instanceMethod(); //인스턴스 메서드 접근, compile error
