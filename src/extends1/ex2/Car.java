6373f829f0a9f3c8d481886ff25dc8a27b64cc4b
Hxxseung <soaorsith2@naver.com>
2024-09-26 12:26:46 +0900

    extends

---
+++ src/extends1/ex2/Car.java
@@ -0,0 +1,7 @@
+package extends1.ex2;
+
+public class Car {  //부모클래스 (슈퍼 클래스) : 상속을 통해 자신의 필드와 메서드를 다른 클래스에 제공하는 클래스
+    public void move(){
+        System.out.println("차를 이동합니다.");
+    }
+}
---
+++ src/extends1/ex3/ElectricCar.java
@@ -0,0 +1,8 @@
+package extends1.ex3;
+
+public class ElectricCar extends Car {   //자식 클래스(서브 클래스): 부모 클래스로부터 필드와 메서드를 상속받는 클래스
+
+    public void charge(){
+        System.out.println("충전합니다.");
+    }
+}
