e209dcd92a42d07bc6f7bbaf49ed4998124e90ee
Hxxseung <soaorsith2@naver.com>
2024-09-26 17:56:16 +0900

    add text

--- src/extends1/overriding/ElectricCar.java
+++ src/extends1/overriding/ElectricCar.java
@@ -2,7 +2,7 @@ package extends1.overriding;
 
 public class ElectricCar extends Car {   //자식 클래스(서브 클래스): 부모 클래스로부터 필드와 메서드를 상속받는 클래스
 
-    @Override
+    @Override   // 부모에게서 상속 받은 기능을 자식이 **재정의 하는 것을 메서드 오버라이딩(Overriding)
     public void move(){
         System.out.println("전기차를 빠르게 이동합니다.");
     }
