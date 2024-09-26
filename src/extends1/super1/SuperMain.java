fb3cf7977dcc479b42c601613e14605e0bd310ca
Hxxseung <soaorsith2@naver.com>
2024-09-26 14:28:37 +0900

    super + 문제풀이

---
+++ src/extends1/super1/SuperMain.java
@@ -0,0 +1,11 @@
+package extends1.super1;
+
+public class SuperMain {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.call();
+    }
+}
+
+//this는 본인 타입
+//super는 본인 타입의 상위
