511c0a5d15e2caff92c97ab70a3239e26cc30ef3
Hxxseung <soaorsith2@naver.com>
2024-09-28 16:40:08 +0900

    text add

--- src/poly/ex3/AbstractAnimal.java
+++ src/poly/ex3/AbstractAnimal.java
@@ -2,6 +2,7 @@ package poly.ex3;
 
 public abstract class AbstractAnimal { //class 앞에 abstract를 반드시 붙여야 함.
     public abstract void sound();   //자식이 반드시 오버라이딩 해야함.
+                                    //추상 method는 body를 가질 수 없다.
 
     public void move(){
         System.out.println("동물이 움직입니다.");
